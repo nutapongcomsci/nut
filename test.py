1f91de6dd70a1dac63d0248752159e1083d1bc2a
PanitanAmperePoonkla <48286563+PanitanAmperePoonkla@users.noreply.github.com>
2019-03-22 13:16:23 +0700

    Update test.py
    update line 1-17

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
-def grad_assign(score):
-    if score > 80:
-        return 'เกรด A'
-    elif score > 70:
-        return 'เกรด B'
-    elif score > 60:
-        return 'เกรด C'
-    elif score > 50:
-        return 'เกรด D'
-    else: 'สอบไม่ผ่าน'
+def grad_assign(s):
+    if s>=80:
+        return "A"
+    elif s>=75:
+        return "B+"
+    elif s>=70:
+         return "B"
+    elif s>=65:
+         return "C+"
+    elif s>=60:
+         return "C"
+    elif s>=55:
+         return "D+"
+    elif s>=50:
+         return "D"
+    else :
+         return "F"
 def grade_req(eng, math):
     if eng > 70 and math < 80:
         return True
